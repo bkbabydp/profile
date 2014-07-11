0ceb868e01df68c8d884407ececc24e99887aa77
David Zhang <david.shejio@gmail.com>
2014-07-11 16:39:52 +0800

    init

---
+++ set_ssh.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir ~/.ssh
+cp ./.ssh/authorized_keys ~/.ssh/
+sleep 10
+exit
---
+++ set_vpn.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+yum install pptp pptp-setup
+pptpsetup --create fq --server 107.170.234.24 --username david --password 0.123456789bk --encrypt
+
+pppd call fq
+sleep 10s
+ip route replace default dev ppp0
